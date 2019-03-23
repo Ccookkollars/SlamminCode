cb81ecf574bde98db6d73a70ba099786f4584ae3
Caleb <ccookkollars@gmail.com>
2019-03-22 21:35:56 -0400

    use maven for source control. the xml makes me lose my mind, but it was lose my mind or lose my mind so I lost my mind.

--- src/main/java/org/eclipse/jetty/demo/EventClient.java
+++ src/main/java/org/eclipse/jetty/demo/EventClient.java
@@ -12,7 +12,7 @@ public class EventClient
 {
     public static void main(String[] args)
     {
-        URI uri = URI.create("ws://localhost:8080/events/");
+        URI uri = URI.create("ws://localhost:8080/sock/events/");
 
         try
         {
--- src/main/java/org/eclipse/jetty/demo/EventServer.java
+++ src/main/java/org/eclipse/jetty/demo/EventServer.java
@@ -1,41 +1,56 @@
 package org.eclipse.jetty.demo;
 
+import java.io.File;
+
 import javax.websocket.server.ServerContainer;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
+import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
 
-public class EventServer
-{
-    public static void main(String[] args)
-    {
+public class EventServer {
+    static Logger log = Log.getLogger(EventServer.class);
+
+    public static void main(String[] args) {
         Server server = new Server();
         ServerConnector connector = new ServerConnector(server);
         connector.setPort(8080);
         server.addConnector(connector);
 
-        // Setup the basic application "context" for this application at "/"
-        // This is also known as the handler tree (in jetty speak)
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
+        // Servlet for websockets
+        ServletContextHandler socketContext = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        socketContext.setContextPath("/sockets/");
 
-        try
-        {
+        // Resource handler for serving the game front end
+        ResourceHandler resourceHandler = new ResourceHandler();
+        ContextHandler resourceContext = new ContextHandler();
+        resourceContext.setContextPath("/");
+        File resourceBase = new File("src/main/webapp");
+        resourceContext.setBaseResource(Resource.newResource(resourceBase));
+        resourceContext.setHandler(resourceHandler);
+
+        // Mash those bitches together, we need to do both of them
+        HandlerCollection contexts = new HandlerCollection(socketContext, resourceContext);
+        server.setHandler(contexts);
+
+        try {
             // Initialize javax.websocket layer
-            ServerContainer wscontainer = WebSocketServerContainerInitializer.configureContext(context);
+            ServerContainer wscontainer = WebSocketServerContainerInitializer.configureContext(socketContext);
 
             // Add WebSocket endpoint to javax.websocket layer
             wscontainer.addEndpoint(EventSocket.class);
 
             server.start();
-            server.dump(System.err);
+//            server.dump(System.err); // just kind of annoying
             server.join();
-        }
-        catch (Throwable t)
-        {
+        } catch (Throwable t) {
             t.printStackTrace(System.err);
         }
     }
