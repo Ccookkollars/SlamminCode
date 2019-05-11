614918ce487a015396c0b587d6c515c20a002a85
Caleb <ccookkollars@gmail.com>
2019-05-10 23:02:18 -0400

    dev mode only

--- src/main/webapp/dist/main.js
+++ src/main/webapp/dist/main.js
@@ -1,1 +1,100 @@
-!function(e){var t={};function n(o){if(t[o])return t[o].exports;var r=t[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,n),r.l=!0,r.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)n.d(o,r,function(t){return e[t]}.bind(null,r));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="dist",n(n.s=0)}([function(e,t){webSocket=new WebSocket("ws://98.110.200.185:8080/sockets/events/"),webSocket.onmessage=function(e){console.log("Received data string: "+e)};var n=document.getElementById("canvas");function o(e,t,n,o){this.img=e,this.width=t,this.height=n,this.positions=o}context=n.getContext("2d"),o.prototype={draw:function(e,t,n){var o=this.positions[e];context.drawImage(this.img,o[0],o[1],this.width,this.height,t,n,this.width,this.height)}};var r=new Image;r.src="https://davetayls.me/space-invaders/sprites.png";new o(r,32,16,[[10,523],[131,523],[191,523]]);sprite.draw(0,10,200),sprite.draw(1,50,200),sprite.draw(2,90,200),document.getElementsByTagName("h1")[0].innerHTML="Hello",console.log("Init");!function(e){let t=Object.create(null);function n(n){e.includes(n.key)&&(t[n.key]="keydown"==n.type,n.preventDefault(),webSocket.send(n.key),console.log(n.key))}window.addEventListener("keydown",n),window.addEventListener("keyup",n)}(["ArrowLeft","ArrowRight","ArrowUp","w","a","s","d","W","A","S","D"])}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "dist";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("\n\nwebSocket = new WebSocket(\"ws://98.110.200.185:8080/sockets/events/\");\nwebSocket.onmessage = function(event){\n\tif (typeof event === \"string\"){\n\t\tmessageReceive(event);\n\t}\n}\nfunction messageRecieve(msg){\n\tconsole.log(\"messageReceive: \" + msg);\n\tdocument.getElementsByTagName(\"h1\")[0].innerHTML = msg;\n\tdat = JSON.parse(msg);\n\t\n\tflag = false;\n\tif (\"color\" in dat){\n\t\tdinos[dat.color].x = dat.x;\n\t\tdinos[dat.color].y = dat.y;\n\t}\n\telse{\n\t\tdinos[\"blue\"].x = dat.x;\n\t\tdinos[\"blue\"].y = dat.y;\n\t}\n}\nvar canvas = document.getElementById('canvas');\nctx = canvas.getContext('2d');\n\nfunction trackKeys(keys) {\n  let down = Object.create(null);\n  function track(event) {\n    if (keys.includes(event.key)) {\n      down[event.key] = event.type == \"keydown\";\n      event.preventDefault();\n      webSocket.send(event.key);\n      console.log(event.key);\n    }\n  }\n  window.addEventListener(\"keydown\", track);\n  window.addEventListener(\"keyup\", track);\n\n  return down;\n}\nfunction sendDat() {\n  webSocket.send();\n}\nfunction Sprite(img, srcwidth, srcheight, width, height, positions){\n  this.img = img;\n  this.srcwidth = srcwidth;\n  this.srcheight = srcheight;\n  this.width = width;\n  this.height = height;\n  this.positions = positions;\n}\n\nSprite.prototype = {\n\tdraw: function(position, x, y){\n\t\tvar pos = this.positions[position];\n\t\tctx.drawImage(\n\t\t\t\tthis.img,\n\t\t\t\tpos[0],\n\t\t\t\tpos[1],\n\t\t\t\tthis.srcwidth,\n\t\t\t\tthis.srcheight,\n\t\t\t\tx, y,\n\t\t\t\tthis.width,\n\t\t\t\tthis.height\n\t\t);\n\t}\n};\nfunction Dino(path, startframe, endframe, x = 0, y = 0){\n\tthis.x = x;\n\tthis.y = y;\n\tthis.path = path;\n\tthis.lastframe = startframe;\n\tthis.startframe = startframe;\n\tthis.endframe = endframe;\n\tthis.img = new Image();\n\tthis.img.src = path;\n\tvar loc = []; \n\tfor(let i=this.startframe; i<this.endframe;i++){\n\t    loc[i] = [i*24, 0];\n\t}\n\tthis.sprite = new Sprite(this.img, 24, 24, 48, 48, loc);\n}\n\nDino.prototype = {\n\tdraw: function(){\n\t\tthis.sprite.draw(this.lastframe++, this.x, this.y);\n\t\tif (this.lastframe >= this.endframe){\n\t\t\tthis.lastframe = this.startframe; \n\t\t}\n\t}\n}\n\ndinos = {\n\t\"blue\": new Dino('res/sheets/DinoSprites - doux.png', 4, 10),\n\t\"red\": new Dino('res/sheets/DinoSprites - mort.png', 4, 10, 300, 300)\n} \n\nsetInterval(() => {\n\tctx.mozImageSmoothingEnabled = false;\n\tctx.webkitImageSmoothingEnabled = false;\n\tctx.msImageSmoothingEnabled = false;\n\tctx.imageSmoothingEnabled = false;\n\tctx.clearRect(0, 0, canvas.width, canvas.height);\n\tfor (const [color, dino] of Object.entries(dinos)) {\n\t  dino.draw();\n\t}\n}, 200);\n\nsetTimeout(() => {\n\tmessageRecieve(\"{\\\"x\\\":50, \\\"y\\\":10}\");\n}, 700);\nsetTimeout(() => {\n\tmessageRecieve(\"{\\\"color\\\":\\\"red\\\", \\\"x\\\":325, \\\"y\\\":318}\");\n}, 1500);\n\ndocument.getElementsByTagName(\"h1\")[0].innerHTML =\"Hello\";\nconsole.log(\"Init\");\nconst arrowKeys = trackKeys([\"ArrowLeft\", \"ArrowRight\", \"ArrowUp\", \"w\", \"a\", \"s\", \"d\", \"W\", \"A\", \"S\", \"D\"]);\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ })
+
+/******/ });
